326d407a78b6c61b288cf666fec0d2992b206cf3
Erlend Graff <erlend.graff@gmail.com>
2018-12-08 18:12:26 +0100

    Travis: debug after_script not being run

--- ci/travis-install-linux.sh
+++ ci/travis-install-linux.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-# Exit on failure
-set -e
+# Exit on failure, verbose
+set -ev
 
 wget https://github.com/scottkosty/install-tl-ubuntu/raw/master/install-tl-ubuntu && chmod +x ./install-tl-ubuntu
+
 sudo apt-get update -qq
 sudo apt-get install --fix-missing
 sudo apt-get install --no-install-recommends -qq poppler-utils ghostscript imagemagick --fix-missing
@@ -17,7 +18,6 @@ sudo ./install-tl-ubuntu --only-apt-and-dpkg
 sudo wget http://mirror.utexas.edu/ctan/systems/texlive/tlnet/install-tl-unx.tar.gz -O install-tl-unx.tar.gz
 tar -xf "install-tl-unx.tar.gz"
 export tl_dir=$( ls | grep -P "install-tl-\d{8}$" | head -n 1 )
-
 
 cd "${tl_dir}"
 echo "i" | sudo -s ./install-tl -logfile install-tl.log -repository http://mirrors.rit.edu/CTAN/systems/texlive/tlnet -profile ../texlive.profile
