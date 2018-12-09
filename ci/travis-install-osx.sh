20d61d11a1789fa194d7f40643184c17cdcd725d
Erlend Graff <erlend.graff@gmail.com>
2018-12-09 19:27:44 +0100

    Travis: debug macOs build

--- ci/travis-install-osx.sh
+++ ci/travis-install-osx.sh
@@ -3,31 +3,6 @@
 # Exit on failure
 set -e
 
-sw_vers
-
-brew update
-
-brew uninstall --ignore-dependencies python
-brew install python@2 --universal --framework
-export PATH="$(brew --prefix python@2)/bin:$PATH"
-python2.7 -v || (exit 0)
-python2 -v || (exit 0)
-python -v
-
-brew install libidn
-brew install ghostscript
-gs -v
-
-brew install imagemagick
-compare -version
-
-# Qt is xpdf dependency
-# We specify version explicitly, because Qt >= 5.12.0 requires macOs >= 10.12.0
-brew install qt@5.11
-
-brew install xpdf
-pdfinfo -v || (exit 0)
-
 brew cask install -v basictex
 # export PATH=/Library/TeX/Distributions/.DefaultTeX/Contents/Programs/texbin:$PATH
 export PATH=/Library/TeX/Distributions/Programs/texbin:$PATH
